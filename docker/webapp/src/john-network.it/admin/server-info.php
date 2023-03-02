3841f003eb883a21f3c4063a2d980e6eac0bbfff
Lukas Poggemann <113536547+lukas-pgm@users.noreply.github.com>
2023-03-02 16:39:51 +0100

    Update server-info.php

--- docker/webapp/src/john-network.it/admin/server-info.php
+++ docker/webapp/src/john-network.it/admin/server-info.php
@@ -34,7 +34,7 @@
 </head>
 <body>
 <div id="app" class="app">
-<?php include("../config/includes/sidebar.user.php"); ?>
+<?php include("../../config/includes/sidebar.user.php"); ?>
   <div id="content" class="app-content">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="<?php echo getDefaultURL(); ?>">HOME</a></li>
@@ -45,7 +45,7 @@
 
     <a href="#" data-toggle="scroll-to-top" class="btn-scroll-top fade"><i class="fa fa-arrow-up"></i></a>
   </div>
-<?php include("../config/includes/footer.php"); ?>
+<?php include("../../config/includes/footer.php"); ?>
 <script src="<?php echo getAssetsURL(); ?>js/status.js"></script>
 </body>
 </html>
