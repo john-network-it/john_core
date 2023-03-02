080f4ad71fe3effc1957b8dcf97da4d04201bd9b
Lukas Poggemann <113536547+lukas-pgm@users.noreply.github.com>
2023-03-02 18:17:23 +0100

    Update server-info.php

--- docker/webapp/src/john-network.it/admin/server-info.php
+++ docker/webapp/src/john-network.it/admin/server-info.php
@@ -61,9 +61,9 @@
             #phpinfo h1 {font-size: 150%;}
             #phpinfo h2 {font-size: 125%;}
             #phpinfo .p {text-align: left;}
-            #phpinfo .e {background-color: #ccf; width: 300px; font-weight: bold;}
-            #phpinfo .h {background-color: #99c; font-weight: bold;}
-            #phpinfo .v {background-color: #ddd; max-width: 300px; overflow-x: auto; word-wrap: break-word;}
+            #phpinfo .e {width: 300px; font-weight: bold;}
+            #phpinfo .h {font-weight: bold;}
+            #phpinfo .v {max-width: 300px; overflow-x: auto; word-wrap: break-word;}
             #phpinfo .v i {color: #999;}
             #phpinfo img {float: right; border: 0;}
             #phpinfo hr {width: 934px; background-color: #ccc; border: 0; height: 1px;}
